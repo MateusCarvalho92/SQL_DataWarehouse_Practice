7ecdf15c556f8e120f214e5dfbf0529b785e200b
MateusCarvalho92 <Mateus_Carvalho92@hotmail.com>
2026-01-10 20:45:03 -0300

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -11,6 +11,7 @@ CREATE TABLE silver.crm_cust_info (
 	dwh_create_date DATETIME2 DEFAULT GETDATE() -- data engineer metadata
 );
 
+/*				OLD DDL
 IF OBJECT_ID ('silver.crm_prd_info' , 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
@@ -22,8 +23,23 @@ CREATE TABLE silver.crm_prd_info (
 	prd_start_dt DATETIME,
 	prd_end_dt DATETIME,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
-);
+);	*/
 
+-- Updated DDL for silver.crm_prd_info after Cleaning the data
+IF OBJECT_ID ('silver.crm_prd_info' , 'U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info;
+CREATE TABLE silver.crm_prd_info (
+	prd_id INT,
+	cat_id NVARCHAR(50),
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATE,
+	prd_end_dt DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);	
+/*				OLD DDL
 IF OBJECT_ID ('silver.crm_sales_details' , 'U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details;
 CREATE TABLE silver.crm_sales_details(
@@ -31,8 +47,24 @@ CREATE TABLE silver.crm_sales_details(
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
 	sls_order_dt INT,
-	sls_shit_dt INT,
+	sls_ship_dt INT,
 	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);	*/
+
+-- Updated DDL for silver.crm_sales_details after Cleaning the data
+IF OBJECT_ID ('silver.crm_sales_details' , 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details(
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT,
